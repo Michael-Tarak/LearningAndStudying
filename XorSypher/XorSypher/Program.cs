72d6a1c6fb26e787951d88d6c44c300c5adf6a0b
Michael-Tarak <Tarakanovma98@gmail.com>
2020-08-10 01:51:29 +0300

    xorend

--- XorSypher/XorSypher/Program.cs
+++ XorSypher/XorSypher/Program.cs
@@ -11,11 +11,12 @@ namespace XorSypher
             Console.InputEncoding = Encoding.Unicode;
             Console.OutputEncoding = Encoding.Unicode;
 
-            var inputTextFile = "encodeInput.txt";
+            var encodeTextFileName = "encodeInput.txt";
+            var decodeTextFileName = "encodeOutput.txt";
             var alphabetCryptFile = "alphabet.txt";
             while (true)
             {
-                if (!File.Exists(inputTextFile))
+                if (!File.Exists(encodeTextFileName))
                 {
                     Console.WriteLine("Не найден файл с текстом!");
                     break;
@@ -26,13 +27,93 @@ namespace XorSypher
                     break;
                 }
 
-                var textToEncode = File.ReadAllText(inputTextFile).ToLower().Trim();
-                var alphabetFromFile = File.ReadAllLines(alphabetCryptFile);
-                var symbolAlphabet = new string [alphabetFromFile.Length];
-                var gammedAlphabet = new string [alphabetFromFile.Length];
+                var textToEncode = File.ReadAllText(encodeTextFileName).ToLower().Trim();
 
-                Console.Write("Введите гамму (двоичная):");
-                var gamma = Console.ReadLine();
+                var alphabetFromFile = File.ReadAllLines(alphabetCryptFile);
+                var symbolAlphabet = new char[alphabetFromFile.Length];
+                var gammedAlphabet = new string[alphabetFromFile.Length];
+
+                for (int i = 0; i < alphabetFromFile.Length; i++)
+                {
+                    symbolAlphabet[i] = alphabetFromFile[i][0];
+                    gammedAlphabet[i] = alphabetFromFile[i].Substring(2).Trim();
+                    //Console.WriteLine($"{symbolAlphabet[i]},{gammedAlphabet[i]}");
+                }
+
+                for (int i = 1; i < gammedAlphabet.Length; i++)
+                {
+                    if (gammedAlphabet[i].Length != gammedAlphabet[i - 1].Length)
+                    {
+                        Console.WriteLine($"Длина гаммы {i + 1}-го символа отлична от предыдущего!");
+                    }
+                }
+
+                Console.Write("Введите ключ (двоичный): ");
+                var gammaKey = Console.ReadLine().Trim();
+                string gammedFileText = "";
+
+                for (int j = 0; j < textToEncode.Length; j++)
+                {
+                    for (int i = 0; i < symbolAlphabet.Length; i++)
+                    {
+                        if (textToEncode[j] == symbolAlphabet[i])
+                        {
+                            gammedFileText += gammedAlphabet[i];
+                            break;
+                        }
+                        else if (i == symbolAlphabet.Length)
+                        {
+                            Console.WriteLine($"{j + 1}-й символ в тексте отсутствует в алфавите!");
+                        }
+                    }
+                }
+
+                var extendableGammaKey = gammaKey;
+
+                while (gammedFileText.Length > extendableGammaKey.Length)
+                {
+                    extendableGammaKey += gammaKey;
+                }
+                if (extendableGammaKey.Length > gammedFileText.Length)
+                {
+                    extendableGammaKey = extendableGammaKey.Substring(0, gammedFileText.Length);
+                }
+
+                string binarTextXORResult = "";
+
+                for (int i = 0; i < gammedFileText.Length; i++)         //XOR
+                {
+                    if (extendableGammaKey[i] == gammedFileText[i])
+                    {
+                        binarTextXORResult += "0";
+                    }
+                    else
+                    {
+                        binarTextXORResult += "1";
+                    }
+                }
+
+                int gammaMarker = 0;
+                var alphabetSymbolEncodingLength = gammedAlphabet[0].Length;
+                string outputText = "";
+
+                while (gammaMarker < binarTextXORResult.Length)
+                {
+                    var pieceOfXOR = binarTextXORResult.Substring(gammaMarker, alphabetSymbolEncodingLength);
+                    //Console.Write(pieceOfXOR);
+                    for (int i = 0; i < gammedAlphabet.Length; i++)
+                    {
+                        if (pieceOfXOR.Equals(gammedAlphabet[i]))
+                        {
+                            //Console.WriteLine(symbolAlphabet[i]);
+                            outputText += symbolAlphabet[i];
+                            break;
+                        }
+                    }
+                    gammaMarker += alphabetSymbolEncodingLength;
+                }
+                File.WriteAllText(decodeTextFileName, outputText);
+                Console.WriteLine($"Вывод: {outputText}");
             }
         }
     }
