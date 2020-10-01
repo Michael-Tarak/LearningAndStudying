741766c700943750171744ce0e025483f4ac2968
Michael-Tarak <Tarakanovma98@gmail.com>
2020-10-02 00:19:41 +0300

    vigenereclassed

--- Encryption3/Encryption3/Program.cs
+++ Encryption3/Encryption3/Program.cs
@@ -10,18 +10,13 @@ namespace Encryption3
         {
             Console.InputEncoding = Encoding.Unicode;
             Console.OutputEncoding = Encoding.Unicode;
-            string alphabet = "";
-            if (File.Exists("alphabet.txt"))
-            {
-                alphabet = File.ReadAllText("alphabet.txt");
-            }
-            else
-            {
-                Console.WriteLine("Не найден файл с алфавитом!");
-            }
             while (true)
             {
-                Console.WriteLine("Выберите действие:\n1 - шифрование\n2 - дешифрование\nДля выхода введите \"exit\"");
+                Console.Write("Введите текст: ");
+                var text = Console.ReadLine().ToLower().Trim();
+                Console.Write("Введите ключ: ");
+                var key = Console.ReadLine().ToLower().Trim();
+                Console.WriteLine("Выберите действие:\n1 - шифрование\n2 - дешифрование");
                 var input = Console.ReadLine().ToLower();
                 if (input == "exit")
                 {
@@ -30,138 +25,16 @@ namespace Encryption3
                 switch (input)
                 {
                     case "1":
-                        Encryption(alphabet);
+                        Console.WriteLine($"Вывод: {Vigenere.Encrypt(text, key, Vigenere.Alphabet)}");
                         break;
                     case "2":
-                        Decryption(alphabet);
+                        Console.WriteLine($"Вывод: {Vigenere.Encrypt(text,key,Vigenere.Alphabet,true)}");
                         break;
                     default:
                         Console.WriteLine("Неверная команда!");
                         break;
                 }
             }
-            static void Encryption(string alphabet)
-            {
-                try
-                {
-                    string keyWordInput = "";
-                    while (true)
-                    {
-                        Console.Write("Введите шифрующее слово(латиница):");
-                        keyWordInput = Console.ReadLine().ToLower();
-                        if (!string.IsNullOrWhiteSpace(keyWordInput))
-                        {
-                            break;
-                        }
-                        Console.WriteLine("Некорректное слово!");
-                    }
-                    var keyAppendable = new StringBuilder("");
-                    var inputAlphabet = new StringBuilder(alphabet);
-                    var inputText = new StringBuilder(File.ReadAllText("encryptionInput.txt").ToLower());
-                    var outputText = new StringBuilder("");
-                    while (keyAppendable.Length < inputText.Length)
-                    {
-                        keyAppendable.Append(keyWordInput);
-                    }
-                    if (keyAppendable.Length > inputText.Length)
-                    {
-                        keyAppendable.Remove(inputText.Length, keyAppendable.Length % inputText.Length);
-                    }
-                    var step = new int[keyAppendable.Length];
-                    for (int i = 0; i < step.Length; i++)
-                    {
-                        for (int j = 0; j < alphabet.Length; j++)
-                        {
-                            if (keyAppendable[i] == alphabet[j])
-                            {
-                                step[i] = j;
-                                break;
-                            }
-                        }
-                    }
-                    for (int i = 0; i < inputText.Length; i++)
-                    {
-                        for (int j = 0; j < alphabet.Length; j++)
-                        {
-                            if (inputText[i] == alphabet[j])
-                            {
-                                outputText.Append(alphabet[(j + step[i]) % alphabet.Length]);
-                            }
-                        }
-                    }
-                    Console.WriteLine($"Ввод:  {inputText}");
-                    Console.WriteLine($"Вывод: {outputText}");
-                    File.WriteAllText("encryptionOutput.txt", outputText.ToString());
-                }
-                catch (IOException)
-                {
-                    Console.WriteLine("Файл не найден!");
-                }
-            }
-            static void Decryption(string alphabet)
-            {
-                try
-                {
-                    string keyWordInput = "";
-                    while (true)
-                    {
-                        Console.Write("Введите шифрующее слово(латиница):");
-                        keyWordInput = Console.ReadLine().ToLower();
-                        if (!string.IsNullOrWhiteSpace(keyWordInput))
-                        {
-                            break;
-                        }
-                        Console.WriteLine("Некорректное слово!");
-                    }
-                    var keyAppendable = new StringBuilder("");
-                    var inputAlphabet = new StringBuilder(alphabet);
-                    var inputText = new StringBuilder(File.ReadAllText("encryptionOutput.txt").ToLower());
-                    var outputText = new StringBuilder("");
-                    while (keyAppendable.Length < inputText.Length)
-                    {
-                        keyAppendable.Append(keyWordInput);
-                    }
-                    if (keyAppendable.Length > inputText.Length)
-                    {
-                        keyAppendable.Remove(inputText.Length, keyAppendable.Length - inputText.Length);
-                    }
-                    var step = new int[keyAppendable.Length];
-                    for (int i = 0; i < step.Length; i++)
-                    {
-                        for (int j = 0; j < alphabet.Length; j++)
-                        {
-                            if (keyAppendable[i] == alphabet[j])
-                            {
-                                step[i] = j;
-                                break;
-                            }
-                        }
-                    }
-                    for (int i = 0; i < inputText.Length; i++)
-                    {
-                        for (int j = 0; j < alphabet.Length; j++)
-                        {
-                            if (inputText[i] == alphabet[j])
-                            {
-                                var move = (j - step[i]);
-                                if (move < 0)
-                                {
-                                    move = (move + (-move / inputAlphabet.Length + 1) * inputAlphabet.Length) % inputAlphabet.Length;
-                                }
-                                outputText.Append(inputAlphabet[move]);
-                            }
-                        }
-                    }
-                    Console.WriteLine($"Ввод:  {inputText}");
-                    Console.WriteLine($"Вывод: {outputText}");
-                    File.WriteAllText("decryptionOutput.txt", outputText.ToString());
-                }
-                catch (IOException)
-                {
-                    Console.WriteLine("Файл не найден!");
-                }
-            }
-
         }
     }
 }
---
+++ Encryption3/Encryption3/Vigenere.cs
@@ -0,0 +1,52 @@
+﻿namespace Encryption3
+{
+    static class Vigenere
+    {
+        private static readonly string alphabet = "abcdefghijklmnopqrstuvwxyz0123456789.,!@#$%^&*()_+=-{}[]<>?;:";
+
+        public static string Alphabet => alphabet;
+        public static string Encrypt(string text, string key, string alphabet, bool isDecryption = false)
+        {
+            var result = "";
+
+            var step = new int[key.Length];
+            for (int i = 0; i < step.Length; i++)
+            {
+                for (int j = 0; j < alphabet.Length; j++)
+                {
+                    if (key[i] == alphabet[j])
+                    {
+                        if (isDecryption)
+                        {
+                            step[i] = -j;
+                        }
+                        else
+                        {
+                            step[i] = j;
+                        }
+                        
+                        break;
+                    }
+                }
+            }
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                for (int j = 0; j < alphabet.Length; j++)
+                {
+                    if (text[i] == alphabet[j])
+                    {
+                        var move = (j + step[i % key.Length]);
+                        if (move < 0)
+                        {
+                            move += alphabet.Length;
+                        }
+                        result += alphabet[move % alphabet.Length];
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
