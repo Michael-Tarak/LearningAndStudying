7dddc44fbe4daa8fc7b557ae1898b9adf2373008
Michael-Tarak <Tarakanovma98@gmail.com>
2023-05-27 13:45:31 +0300

    twosumcreated

--- TwoSum/src/Main.java
+++ TwoSum/src/Main.java
@@ -1,19 +1,22 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-
-
+        var sol = new Solution();
+        System.out.println(Arrays.toString(sol.twoSum(new int []{2,7,11,15},9)));
+        System.out.println(Arrays.toString(sol.twoSum(new int []{3,2,4},6)));
+        System.out.println(Arrays.toString(sol.twoSum(new int []{3,3},6)));
         }
+    }
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        var result = new int [2];
+        for(int i=0;i< nums.length - 1; i++){
+            for (int j = i + 1;j <nums.length; j++){
+                if(nums[i]+nums[j] == target)
+                    result= new int[]{nums[i], nums[j]};
+            }
+        }
+        return result;
     }
 }
